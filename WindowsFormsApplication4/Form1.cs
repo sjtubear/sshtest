6c61fa8d217bf1f90bbcec822a547daa585beb40
sjtubear <wangth25@aliyun.com>
2014-12-18 18:51:28 +0800

    first

---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Application.Exit();
+        }
+    }
+}
